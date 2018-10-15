caaa829a2479a5fcc25574ac730378da57e03531
Daniel Klock <daniel.a.klock@gmail.com>
2018-10-15 13:52:25 +0200

    Add a temporary test for testing tensorforce

---
+++ src/test.py
@@ -0,0 +1,75 @@
+ # examples/quickstart.py
+
+import numpy as np
+
+from tensorforce.agents import PPOAgent
+from tensorforce.agents.learning_agent import LearningAgent
+from tensorforce.execution import Runner
+from tensorforce.contrib.openai_gym import OpenAIGym
+
+
+# Create an OpenAIgym environment
+#CartPole-v0
+env = OpenAIGym('BipedalWalker-v2', visualize=False)
+
+# Network as list of layers
+network_spec = [
+    dict(type='dense', size=32, activation='tanh'),
+    dict(type='dense', size=32, activation='tanh')
+]
+
+batch_size = 4096
+
+agent = PPOAgent(
+    states=env.states,
+    actions=env.actions,
+    network=network_spec,
+    # PPOAgent
+    step_optimizer=dict(
+        type='adam',
+        learning_rate=1e-3
+    ),
+    optimization_steps=10,
+    # Model
+    scope='ppo',
+    discount=0.99,
+    # DistributionModel
+    entropy_regularization=0.01,
+    # PGModel
+    baseline_mode=None,
+    baseline=None,
+    baseline_optimizer=None,
+    gae_lambda=None,
+    # PGLRModel
+    likelihood_ratio_clipping=0.2
+)
+
+# agent = LearningAgent(
+#     states=env.states,
+#     actions=env.actions,
+#     network=network_spec,
+#     update_mode={'unit': "timesteps", 'batch_size': 64, 'frequency': 4},
+#     memory={'type': 'replay', 'include_next_states': True, 'capacity': 1000*batch_size},
+#     optimizer={'type': 'adam', 'learning_rate': 1e-3}
+# )
+
+# Create the runner
+runner = Runner(agent=agent, environment=env)
+
+
+# Callback function printing episode statistics
+def episode_finished(r):
+    print("Finished episode {ep} after {ts} timesteps (reward: {reward})".format(ep=r.episode, ts=r.episode_timestep,
+                                                                                 reward=r.episode_rewards[-1]))
+    return True
+
+
+# Start learning
+runner.run(episodes=300000, max_episode_timesteps=200, episode_finished=episode_finished)
+runner.close()
+
+# Print statistics
+print("Learning finished. Total episodes: {ep}. Average reward of last 100 episodes: {ar}.".format(
+    ep=runner.episode,
+    ar=np.mean(runner.episode_rewards[-100:]))
+)
