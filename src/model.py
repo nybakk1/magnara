f397d51ce27770ad3edccea160012bb3b3fffdad
Roy Åne Sylthe <royaane@gmail.com>
2018-10-31 13:40:07 +0100

    Add run and discretize methods

--- src/model.py
+++ src/model.py
@@ -5,24 +5,24 @@ from collections import deque
 
 import gym as G
 import numpy as np
+import math
 import random
 import matplotlib.pyplot as plt
 
-bat_size = 64
-episodes = 800
-timesteps = 500
 
 class DeepQAgent():
-    def __init__(self, action_space, observation_space):
+    def __init__(self, env, buckets=(10, 12, 10, 12)):
         # hyperparameters
         self.memory = deque(maxlen=2000)
+        self.buckets = buckets
+        self.env = env
         self.gamma = 0.99  # was .95
         self.epsilon = 1.0
         self.epsilon_min = 0.01  # ?
         self.epsilon_decay = 0.995  # ?
         self.learning_rate = 1e-3
-        self.action_space = action_space
-        self.observation_space = observation_space
+        self.action_space = self.env.action_space.n
+        self.observation_space = self.env.observation_space.shape[0]
         self.model = self.build_model()
 
     def build_model(self):
@@ -32,6 +32,9 @@ class DeepQAgent():
         model.add(Dense(self.action_space, activation='linear'))
         model.compile(optimizer=Adam(lr=self.learning_rate), loss='mse')
         return model
+
+    def discretize(self, state):
+        upper = []
 
     # lagrer staten i minnet
     def save_state(self, state, action, reward, done, next_state):
@@ -58,35 +61,38 @@ class DeepQAgent():
                 target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
             target_f = self.model.predict(state)
             target_f[0][action] = target
-            self.model.fit(state,target_f,epochs=1, verbose=0)
+            self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
+    def run(self, episodes=500, timesteps=200, bat_size=32):
+        scores = []
+        for e in range(episodes):
+            state = env.reset()
+
+            state = np.reshape(state, [1, self.observation_space])
+            for ts in range(timesteps):
+                # env.render()
+                action = agent.perform_action(state)
+                next_state, reward, done, _ = self.env.step(action)
+                reward = reward if not done else -10
+                next_state = np.reshape(next_state, [1, self.observation_space])
+                agent.save_state(state, action, reward, done, next_state)
+                state = next_state
+                if done or ts >= timesteps:
+                    scores.append(ts)
+                    print(f'Episode {e+1}/{episodes}, score: {ts}')
+                    break
+            agent.train(bat_size)
+
+        x = [i for i in range(episodes)]
+        z = np.polyfit(x, scores, 8)
+        f = np.poly1d(z)
+        y_new = f(x)
+        plt.plot(x, scores, 'o', x, y_new)
+        plt.show()
+
 
 env = G.make('CartPole-v1')
-agent = DeepQAgent(env.action_space.n, env.observation_space.shape[0])
-scores = []
-for e in range(episodes):
-    state = env.reset()
-
-    state = np.reshape(state, [1, env.observation_space.shape[0]])
-    for ts in range(timesteps):
-        # env.render()
-        action = agent.perform_action(state)
-        next_state, reward, done, _ = env.step(action)
-        reward = reward if not done else -10
-        next_state = np.reshape(next_state, [1, env.observation_space.shape[0]])
-        agent.save_state(state,action,reward,done,next_state)
-        state = next_state
-        if done:
-            scores.append(ts)
-            print('Episode {}/{}, score: {}'.format(e+1, episodes, ts))
-            break
-    agent.train(bat_size)
-
-x = [i for i in range(episodes)]
-z = np.polyfit(x, scores, 8)
-f = np.poly1d(z)
-y_new = f(x)
-plt.plot(x,scores,'o',x,y_new)
-plt.show()
+agent = DeepQAgent(env)
+agent.run()
