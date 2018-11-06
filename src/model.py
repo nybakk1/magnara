6a0f9f93ba3fe04faab860148e55153aad7ea0a9
Daniel Klock <daniel.a.klock@gmail.com>
2018-11-06 13:06:00 +0100

    Implement growing discount factor.

--- src/model.py
+++ src/model.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 # Force TensorFlow to run on CPU. Comment out these lines
 # if you don't use TensorFlow-GPU
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 episodes = 500
@@ -22,16 +22,20 @@ average_size = 100
 
 
 class DeepQAgent:
-    def __init__(self, env, buckets=(6, 12, 6, 12)):
+    def __init__(self, env, buckets=(1, 1, 6, 12)):
         # hyperparameters
         self.memory = deque(maxlen=2000)
         self.buckets = buckets
         self.env = env
-        self.gamma = .99  # was .95
-        self.epsilon = 1.0
-        self.epsilon_min = 0.01  # ?
-        self.epsilon_decay = 0.995  # ?
-        self.learning_rate = 1e-3
+
+        self.discount_factor = 0.1      # Increases as episodes go on to weight later actions less.
+        self.discount_fact_inc = 0.002  # How much discount factor increases.
+        self.discount_fact_max = 1.0    # Maximum limit for discount factor.
+        self.epsilon = 1.0              # Chance to explore.
+        self.epsilon_min = 0.01         # Minimum chance to explore.
+        self.epsilon_decay = 0.995      # Exploration decay factor.
+        self.learning_rate = 1e-3       # Learning rate.
+
         self.action_space = self.env.action_space.n
         self.observation_space = self.env.observation_space.shape[0]
         self.model = self.build_model()
@@ -57,7 +61,7 @@ class DeepQAgent:
         self.memory.append((state, action, reward, done, next_state))
 
     # bestemmer og utfører den action som velges.
-    def perform_action(self, state):
+    def policy(self, state):
         # TODO: Random Action Probability (Read: RAP)
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_space)
@@ -74,44 +78,42 @@ class DeepQAgent:
         for state, action, reward, done, next_state in minibatch:
             target = reward
             if not done:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+                target = reward + self.discount_factor * np.amax(self.model.predict(next_state)[0])
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+        self.discount_factor = min(self.discount_factor + self.discount_fact_inc, self.discount_fact_max)
 
     def run(self, episodes=500, timesteps=200, batch_size=32, average_size=50):
-        scores = []
-        rolling_average = []
+        scores = []             # Save scores to calculate average scores.
+        rolling_average = []    # Save average score for plotting.
         for e in range(episodes):
             state = self.env.reset()
-
             state = np.reshape(state, [1, self.observation_space])
             for ts in range(timesteps):
                 # env.render()
-                action = agent.perform_action(state)
-                next_state, reward, done, _ = self.env.step(action)
-                # next_state = next_state
-                reward = reward if not done else -10
+                action = self.policy(state)
+                next_state, reward, done, _ = self.env.step(action)     # Do the action
+                reward = reward if not done else -timesteps             # Punish for losing.
                 next_state = np.reshape(next_state, [1, self.observation_space])
-                # print(state, next_state)
-                agent.save_state(state, action, reward, done, next_state)
+                self.save_state(state, action, reward, done, next_state)
+
                 state = next_state
                 if done or ts >= timesteps - 1:
                     scores.append(ts)
                     if e > average_size:
                         average = np.average(scores[e-average_size:e])
-                        print(f'Episode {e+1}/{episodes}, average: {average}')
+                        print(f'Episode {e + 1}/{episodes}, average: {average}')
                         rolling_average.append(average)
-                    # print(f'Episode {e+1}/{episodes}, score: {ts}')
                     break
-            agent.train(batch_size)
+            self.train(batch_size)
 
         # Plot rolling average
         x = [i+average_size for i in range(len(rolling_average))]
         plt.plot(x, rolling_average)
-        plt.title("Rolling average of past " + str(average_size) + " episodes.")
+        plt.title(f"Rolling average of past {average_size} episodes.")
         plt.ylabel("Average score")
         plt.xlabel("Episodes")
         plt.show()
