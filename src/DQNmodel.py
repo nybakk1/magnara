d5d885ee97011dba77af8021a22d9782d0b2fe86
Roy Åne Sylthe <royaane@gmail.com>
2018-11-19 13:08:05 +0100

    Refactor DQNmodel
    Minor changes and addition of code.
    
    - Decreased memory but increased batch size
    
    - Epsilon changed from a constant to a logarithmic variable
    
    - Print score for each episode

--- src/DQNmodel.py
+++ src/DQNmodel.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, activations
+from keras.layers import Dense
 from keras.optimizers import Adam
 from collections import deque
 
@@ -14,10 +14,10 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # episodes = 2000
 # max_score = 500
-# batch_size = 32
+# batch_size = 64
 # average_size = 100
 render = False
-when_should_the_code_render_the_cart_pole_v1 = 500
+when_should_the_code_render_the_cart_pole_v1 = 200
 
 # Increase batch-size
 inc_every_episode = 50
@@ -25,19 +25,20 @@ batch_size_inc = 10
 
 
 class DeepQAgent:
-    def __init__(self, env):
+    def __init__(self, env, episodes=1000):
         # hyperparameters
-        self.memory = deque(maxlen=2000)
+        self.memory = deque(maxlen=1000)
         self.env = env
 
+        self.episodes = episodes
         self.discount_factor = 0.1      # Increases as episodes go on to weight later actions less.
         self.discount_fact_inc = 0.002  # How much discount factor increases.
         self.discount_fact_max = 1.0    # Maximum limit for discount factor.
         self.epsilon = 1.0              # Chance to explore.
         self.epsilon_min = 0.01         # Minimum chance to explore.
-        self.epsilon_decay = 0.995      # Exploration decay factor.
-        self.learning_rate = 1e-2       # Learning rate.
-        self.learning_rate_decay = 0.000001  # Learning rate decay
+        self.epsilon_decay = np.e ** (np.log(self.epsilon_min / self.epsilon) / (episodes * 0.8)) # Exploration decay factor.
+        self.learning_rate = 1e-3       # Learning rate.
+        self.learning_rate_decay = 0.00001  # Learning rate decay
 
         self.action_space = self.env.action_space.n
         self.observation_space = self.env.observation_space.shape[0]
@@ -105,7 +106,7 @@ class DeepQAgent:
         """
         if len(self.memory) < batch_size:
             batch_size = len(self.memory)
-        if e % inc_every_episode is 0:
+        if e % inc_every_episode == 0 and e != 0:
             batch_size += batch_size_inc
 
         minibatch = random.sample(self.memory, batch_size)
@@ -120,7 +121,7 @@ class DeepQAgent:
             self.epsilon *= self.epsilon_decay
         self.discount_factor = min(self.discount_factor + self.discount_fact_inc, self.discount_fact_max)
 
-    def run(self, episodes=1000, timesteps=500, batch_size=32, average_size=100, explore=True):
+    def run(self, timesteps=500, batch_size=32, average_size=100, explore=True, run=1):
         """
         Run model, OpenAI gym simulates an environment and the agent starts to learn.
         The rolling average of the scores is plotted at the end.
@@ -129,10 +130,12 @@ class DeepQAgent:
         :param timesteps: positive integer
         :param batch_size: positive integer
         :param average_size: positive integer
+        :param explore: boolean
+        :param run: positive integer
         """
-        scores = []             # Save scores to calculate average scores.
+        scores = deque(maxlen=100)             # Save scores to calculate average scores.
         rolling_average = []    # Save average score for plotting.
-        for e in range(episodes):
+        for e in range(self.episodes):
             state = self.env.reset()
             # state = self.normalize(state)
             state = np.reshape(state, [1, self.observation_space])
@@ -141,28 +144,31 @@ class DeepQAgent:
                     self.env.render()
                 action = self.policy(state, explore)
                 next_state, reward, done, _ = self.env.step(action)     # Do the action
-                reward = reward if not done else -timesteps             # Punish for losing.
+                reward = reward if not done else -timesteps / 2         # Punish for losing.
                 # next_state = self.normalize(next_state)
                 next_state = np.reshape(next_state, [1, self.observation_space])
-                self.save_state(state, action, reward, done, next_state)
+                self.save_state(state, action, reward, done, next_state) if explore else None
 
                 state = next_state
                 if done or ts >= timesteps - 1:
                     scores.append(ts)
                     if e > average_size:
-                        average = np.average(scores[e-average_size:e])
-                        print(f'Episode {e + 1}/{episodes}, average: {average}')
+                        # average = np.average(scores[e-average_size:e])
+                        average = np.mean(scores)
+                        print(f'Run {run}\tEpisode {e + 1}/{self.episodes}\tscore: {ts}\taverage: {average}')
                         rolling_average.append(average)
+                    else:
+                        print(f'Run {run}\tEpisode {e + 1}/{self.episodes}\tscore: {ts}')
                     break
-            self.train(batch_size,e)
+            self.train(batch_size, e) if explore else None
 
         return scores, rolling_average
 
 
 # env = G.make('CartPole-v1')
-# agent = DeepQAgent(env)
-# train = agent.run(episodes, max_score, batch_size, average_size, True)
-# test = agent.run(episodes, max_score, batch_size, average_size, False)
+# agent = DeepQAgent(env, episodes)
+# train = agent.run(episodes, max_score, batch_size, average_size, True, 1)
+# test = agent.run(episodes, max_score, batch_size, average_size, False, 2)
 #
 # from Plot import plot
 # plot(data=[[[i+average_size for i in range(len(train[1]))], train[1]], [[i+average_size for i in range(len(test[1]))], test[1]]],
